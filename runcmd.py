60223225a7882ebbaf35ec1de6228a7acadaa564
Wen Shan Chang <wschang83@yahoo.com.au>
2014-08-20 04:34:21 +1000

    -guard against invalid timeout - round down timeout - comments

--- runcmd.py
+++ runcmd.py
@@ -10,6 +10,9 @@ import traceback
 import signal
 import array
 
+__version_info__ = ('0', '1', '0')
+__version__ = '.'.join(__version_info__)
+
 # defined WindowsError exception for platforms which
 # do not have it
 if not getattr(__builtins__, "WindowsError", None):
@@ -46,12 +49,30 @@ class RunCmdInterruptError(RunCmdError):
 
 
 class _PipeData(threading.Thread):
-    """
+    """ A pipe which continuously reads from a source and writes to a destination file object
+    in a background thread.
 
+    Once the pipe has finished reading, it cannot be restarted. Instead, a
+    new PipeData object must be created.
+
+    Attributes:
+        is_stop     : boolean indicating if the pipe has finished reading from the source.
+        in_fd       : file descriptor representing the input to the pipe. Pass this to the
+                      source of the data to be read
+        dest_file   : the destination file object. This is the file where the data is written out to
+
+        _out_file   : an internal file object representing the output of pipe.
+        _finish_read : boolean to indicate when the pipe has finished reading from the source.
+        _buffer     : a buffer used to temporarily store the chunks of data read from the source.
     """
     CHUNK_SIZE = 1024
 
     def __init__(self, dest_file):
+        """ Constructor
+
+        Args:
+            dest_file: file object where the data will be written into.
+        """
         r, w = os.pipe()
         self._out_file = os.fdopen(r, 'rb')
         self.in_fd = w
@@ -63,12 +84,16 @@ class _PipeData(threading.Thread):
         super(_PipeData, self).__init__()
 
     def start_monitor(self):
+        """ Start reading from the source.
+
+         Users must first set "in_fd" to a source first before calling this.
+        """
         self._finish_read = False
         self.is_stop = False
         self.start()
 
     def run(self):
-        """ Continuously read from pipe and write to the destination file object until
+        """ Continuously read from the source and write to the destination file object until
         user calls stop().
 
         This runs in a background thread.
@@ -82,7 +107,8 @@ class _PipeData(threading.Thread):
         self._finish_read = True
 
     def _read(self):
-        """ Read all buffered input from pipe one chunk at a time and write to pipe.
+        """ Read all content buffered in the pipe one chunk at a time and write to the
+        destination file object.
         """
         read_size = self._out_file.readinto(self._buffer)
         while read_size > 0:
@@ -110,6 +136,27 @@ class _PipeData(threading.Thread):
 
 
 class RunCmd(object):
+    """ Runs a command in a subprocess and wait for it to return or timeout.
+
+    This is a simple replacement the usual subprocess usage pattern:
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        ret, out = p.communicate()
+
+    Except it supports a timeout, where the command can be terminated if it exceeds
+    a given amount of time. It is a blocking call, and will wait for the command
+    to return or timeout.
+
+    Attributes:
+        returncode  : The return code as returned from the process. There are also special
+                      returncode values set if RunCmd encounters issues running the command.
+        cmd         : The command used.
+
+        Error returncodes:
+            INVALID_INPUT_ERR: invalid parameters were used.
+            INTERRUPT_ERR    : command was interrupted, e.g. Keyboard interrupt signal sent
+            TIMEOUT_ERR      : runtime of command has exceed set timeout and was forced to
+                               terminate.
+    """
 
     WAIT_INTERVAL = 0.5
     INVALID_INPUT_ERR = -4
@@ -117,10 +164,23 @@ class RunCmd(object):
     TIMEOUT_ERR = -2
 
     def __init__(self):
+        """ Constructor
+        """
         self.returncode = -1
         self.cmd = ''
 
     def run(self, cmd, timeout=0, shell=False, cwd=None):
+        """ Runs the command and return the return code and output.
+
+        This is similar to Popen.communicate().
+
+        Args:
+            cmd     : command to run.
+            timeout : seconds to wait before terminating command.
+            shell:
+            cwd:
+        :return:
+        """
         f = cStringIO.StringIO()
         # In the event of an exception, we attempt to close the string buffer "f"
         # before raising the exception
@@ -142,7 +202,7 @@ class RunCmd(object):
         if not out_file:
             raise RunCmdInvalidInputError(cmd, 'Error: "out_file" expected a file object, receive None instead')
 
-        timeout = sys.maxint if timeout == 0 else timeout
+        timeout = sys.maxint if int(timeout) <= 0 else int(timeout)
         pipe = _PipeData(out_file)
 
         try:
@@ -222,7 +282,7 @@ class RunCmd(object):
 
 def main():
     cmd = RunCmd()
-    print cmd.run("ls -la", shell=False)
+    print cmd.run("dir", shell=False)
     #print cmd.run('%s -c "print \'Hello World\'"' % sys.executable, shell=False)
 
 
